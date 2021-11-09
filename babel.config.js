4c9f366dc5dbfc6fa70b24460251c569aafa4311
Scott A. Matthews <scott@designsbysm.com>
2021-11-08 19:16:56 -0700

    update lint config

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = api => {
-  const isBabelServer = caller => {
-    return !!(caller && (caller.name === "@babel/cli" || caller.name === "@babel/node"));
-  };
-  const isServer = api.caller(isBabelServer);
   api.cache(false);
 
   const presets = [];
